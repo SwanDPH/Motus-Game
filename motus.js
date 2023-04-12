1b738327e90daa844f89524e535e145c522ef989
SwanDPH <swan.daphne@gmail.com>
2023-04-12 18:02:05 +0200

    MAJ de la mise en forme (couleur charte

--- motus.js
+++ motus.js
@@ -1,11 +1,24 @@
 let joueur1 = new Joueurs("swanou", 27)
 let partie1 = new Parties(joueur1,this,this,this)
-let bouttonLancement = document.querySelector('.valideChoixUser')
+let bouttonValideChoix = document.querySelector('.valideChoixUser')
+let boutonLancementJeu = document.querySelector('.bouttonCommencerLaPartie')
+ 
 
+partie1.afficherIndice()
+console.log(partie1.motADetecter);
 
+console.log(`L'index du random tableau est : ${partie1.indexTabRandom}`);
+console.log(`Le mot est ${(partie1.motADetecter)}`);
+console.log(`L'indice est : ${(partie1.indiceMotADetecter)}`);
 
+boutonLancementJeu.addEventListener('click', function () {
+    joueur1.recuperationDuSurnom()
+    console.log(`Le nom du joueur est ${joueur1.surnom}`);
+    partie1.afficherTabPremiereFois()
+    joueur1.recuperationDuSurnom()
+})
 
-bouttonLancement.addEventListener('click', function () {
+bouttonValideChoix.addEventListener('click', function () {
     partie1.algorithmeDunTour()
 })
 // console.log(`La fonction aGagner() est égal à : ${partie1.aGagne()}`);
--- motusClass.js
+++ motusClass.js
@@ -1,26 +1,43 @@
 class Joueurs {
-    constructor(surnom, age) {
-        this.surnom = surnom
-        this.age = age
+    constructor() {
+        this.surnom = this.recuperationDuSurnom
+    }
+
+    recuperationDuSurnom(){
+        let surnomJoueurHTML = document.querySelector('.nomJoueur').value
+        this.surnom = surnomJoueurHTML
+
+        if (surnomJoueurHTML == "") {
+            alert('Il nous faut impérativement un nom')
+            return false
+        } else {
+            let premierePageJeu = document.querySelector('.premierePage')
+            let deuxiemePageJeu = document.querySelector('.deuxiemePage')
+            premierePageJeu.classList.add('invisible')
+            deuxiemePageJeu.classList.remove('invisible')
+            return true
+        }
     }
 }
 
 
 class Parties {
     constructor(joueurActuel, motADetecter, motDonner, tour) {
-        this.tableauMotsAleatoire = ["Pomme", "chien", "Fleur", "Table", "Faute", "Livre", "Idée", "Oiseau"]
-        this.tableauIndice = ["Fruit, rouge ou vert, comestible, riche en vitamines", "Animal domestique, mammifère, souvent utilisé comme animal de compagnie ou de garde", "Choses colorées, souvent utilisée pour décorer et offrir en cadeau", "Meuble plat, souvent utilisé pour manger, travailler ou étudier", "Erreur, quelque chose de mal fait ou dit", "Objet imprimé, souvent relié, contenant des informations, des histoires ou des instructions", "une pensée ou un concept abstrait dans l'esprit d'une personne", "un animal volant avec des plumes et un bec pointu."]
+        this.tableauMotsAleatoire = ["Pomme", "chien", "Fleur", "Table", "Faute", "Livre", "gazon", "plage"]
+        this.tableauIndice = ["Fruit, rouge ou vert, comestible, riche en vitamines", "Animal domestique, mammifère, souvent utilisé comme animal de compagnie ou de garde", "Choses colorées, souvent utilisée pour décorer et offrir en cadeau", "Meuble plat, souvent utilisé pour manger, travailler ou étudier", "Erreur, quelque chose de mal fait ou dit", "Objet imprimé, souvent relié, contenant des informations, des histoires ou des instructions", "il est souvent tondu en été pour garder une pelouse bien entretenue.", "une étendue de sable ou de galets au bord de la mer, où les gens peuvent se baigner, se détendre ou pratiquer des sports nautiques."]
+        this.indexTabRandom = Math.floor(Math.random()* (this.tableauMotsAleatoire.length - 0) + 0)
+        this.motADetecter = this.tableauMotsAleatoire[this.indexTabRandom].toLowerCase()
+        this.indiceMotADetecter = this.tableauIndice[this.indexTabRandom]
         this.joueurActuel = joueurActuel
-        this.motADetecter = "chien"
         this.motDonner = ""
-        let tabMotADetecter = []
-        let tabMotDonne = []
+        this.tabMotADetecter = []
+        this.tabMotDonne = []
         this.tour = 0
     }
 algorithmeDunTour(){
     let i = 0
     let y = 0
-    this.motDonner = document.querySelector('.promptUser').value
+    this.motDonner = document.querySelector('.promptUser').value.toLowerCase()
     this.tabMotADetecter = []
     this.tabMotDonne = []
     let tourMax = 10
@@ -45,6 +62,7 @@ algorithmeDunTour(){
                         zoneGagne.classList.remove('invisible')
                     } else {
                             while (this.aGagner() != true && this.tour < tourMax) {
+                                this.afficherIndice()
                                 this.nettoyageCase()
                                 
                                 while (i< this.motADetecter.length) {
@@ -74,79 +92,91 @@ algorithmeDunTour(){
                         }
 }
 
-nettoyageCase(){
-    let spanHTML = document.querySelectorAll("span")
-    for (let i = 0; i < spanHTML.length; i++) {
-        spanHTML[i].classList.remove('wellplaced')
-        spanHTML[i].classList.remove('misplaced')
-    }
-}
-    
-comparaisonDeuxTableaux(motADetecter, motDonner){
-    let classHTMLaDetecter = document.querySelectorAll('.lettreMotADetecter')
-    let classHTMDonner = document.querySelectorAll('.lettreMotDonne')
-
-    for (let i = 0; i < motADetecter.length; i++) {
-        if (motDonner[i].includes(motADetecter[i])) {
-            console.log(`Input User LETTRE BIEN PLACE : ${motDonner[i]}`);
-            classHTMDonner[i].classList.add('wellplaced')
-            continue
+        afficherIndice(){
+            let indiceHTML = document.querySelector('.indiceHTML')
+            indiceHTML.innerHTML = this.indiceMotADetecter
         }
 
-        if (motADetecter.includes(motDonner[i]) && motDonner[i].includes(motADetecter[i]) == false) {
-            console.log(`Input User mais lettre MAL PLACE : ${motADetecter[i]}`);
-            classHTMDonner[i].classList.add('misplaced')
-            continue
+        nettoyageCase(){
+            let spanHTML = document.querySelectorAll("span")
+            for (let i = 0; i < spanHTML.length; i++) {
+                spanHTML[i].classList.remove('wellplaced')
+                spanHTML[i].classList.remove('misplaced')
+            }
         }
-    }
-}
+            
+        comparaisonDeuxTableaux(motADetecter, motDonner){
+            let classHTMLaDetecter = document.querySelectorAll('.lettreMotADetecter')
+            let classHTMDonner = document.querySelectorAll('.lettreMotDonne')
 
-afficherLesTableaux(motADetecter, motDonner){
-    let classHTMLaDetecter = document.querySelectorAll('.lettreMotADetecter')
-    let classHTMDonner = document.querySelectorAll('.lettreMotDonne')
+            for (let i = 0; i < motADetecter.length; i++) {
+                if (motDonner[i].includes(motADetecter[i])) {
+                    console.log(`Input User LETTRE BIEN PLACE : ${motDonner[i]}`);
+                    classHTMDonner[i].classList.add('wellplaced')
+                    continue
+                }
 
-    for (let i = 0; i < motADetecter.length; i++) {
-        classHTMLaDetecter[i].innerHTML = motADetecter[i]
-    }
-
-    for (let i = 0; i < motDonner.length; i++) {
-        classHTMDonner[i].innerHTML = motDonner[i]
-    }
-}
-
-tourSuivant(tourMax) {
-    let tourHTML = document.querySelector('.tourActuel')
-    let tourMaxHTML = document.querySelector('.tourMax')
-
-    this.tour++
-    
-    tourHTML.innerHTML = this.tour
-    tourMaxHTML.innerHTML = tourMax
-}
-
-verifValeurMotDonner(){
-    if (this.motDonner.length < 5) {
-        return false
-    }
-    return true
-}
-
-aGagner(){
-    if (this.motDonner == this.motADetecter) {
-        let affichageGagne = document.querySelector('.affichageGagne')
-        let motADetecterHTML = document.querySelector('.motAdeviner')
-        let nbTourHTML = document.querySelector('.nbTourActuel')
-        
-        
-        affichageGagne.classList.remove('invisible')
-        motADetecterHTML.innerHTML= this.motDonner
-        nbTourHTML.innerHTML= this.tour
-
-
-        return true
-    }
-        if (this.tour == 10) {
-            console.log("vous avez perdu");
+                if (motADetecter.includes(motDonner[i]) && motDonner[i].includes(motADetecter[i]) == false) {
+                    console.log(`Input User mais lettre MAL PLACE : ${motADetecter[i]}`);
+                    classHTMDonner[i].classList.add('misplaced')
+                    continue
+                }
+            }
         }
-    }
+
+        afficherLesTableaux(motADetecter, motDonner){
+            let classHTMLaDetecter = document.querySelectorAll('.lettreMotADetecter')
+            let classHTMDonner = document.querySelectorAll('.lettreMotDonne')
+
+            for (let i = 0; i < motADetecter.length; i++) {
+                classHTMLaDetecter[i].innerHTML = motADetecter[i]
+            }
+
+            for (let i = 0; i < motDonner.length; i++) {
+                classHTMDonner[i].innerHTML = motDonner[i]
+            }
+        }
+
+        afficherTabPremiereFois(){
+            let classHTMLaDetecter = document.querySelectorAll('.lettreMotADetecter')
+            for (let i = 0; i < this.motADetecter.length; i++) {
+                classHTMLaDetecter[i].innerHTML = this.motADetecter[i]
+            }
+        }
+
+        tourSuivant(tourMax) {
+            let tourHTML = document.querySelector('.tourActuel')
+            let tourMaxHTML = document.querySelector('.tourMax')
+
+            this.tour++
+            
+            tourHTML.innerHTML = this.tour
+            tourMaxHTML.innerHTML = tourMax
+        }
+
+        verifValeurMotDonner(){
+            if (this.motDonner.length < 5) {
+                return false
+            }
+            return true
+        }
+
+        aGagner(){
+            if (this.motDonner == this.motADetecter) {
+                let affichageGagne = document.querySelector('.affichageGagne')
+                let motADetecterHTML = document.querySelector('.motAdeviner')
+                let nbTourHTML = document.querySelector('.nbTourActuel')
+                
+                
+                affichageGagne.classList.remove('invisible')
+                motADetecterHTML.innerHTML= this.motDonner
+                nbTourHTML.innerHTML= this.tour
+
+
+                return true
+            }
+                if (this.tour == 10) {
+                    console.log("vous avez perdu");
+                }
+            }
 }

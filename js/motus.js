6c16337536ec54344dcc8a8d7f646a4beb86b635
SwanDPH <swan.daphne@gmail.com>
2023-04-19 16:08:18 +0200

    vérification

--- js/motus.js
+++ js/motus.js
@@ -1,27 +1,26 @@
-window.onload = function() {
-let joueur1 = new Joueurs("swanou", 27)
-let partie1 = new Parties(joueur1, this, this, this)
-let boutonLancementJeu = document.querySelector('.bouttonCommencerLaPartie')
-let bouttonValideChoix = document.querySelector('.valideChoixUser')
-let bouttonRejouer = document.querySelector('.rejouer')
-let bouttonQuitter = document.querySelector('.quitterJeu')
+window.addEventListener('load', () => {
+        let joueur1 = new Joueurs("swanou", 27)
+        let partie1 = new Parties(joueur1, this, this, this)
+        let boutonLancementJeu = document.querySelector('.bouttonCommencerLaPartie')
+        let bouttonValideChoix = document.querySelector('.valideChoixUser')
+        let bouttonRejouer = document.querySelector('.rejouer')
+        let bouttonQuitter = document.querySelector('.quitterJeu')
 
-        
-        boutonLancementJeu.addEventListener('click', function () {
-            joueur1.recuperationDuSurnom()
-            partie1.afficherIndice()
-            partie1.afficherTabPremiereFois()
-        })
+                boutonLancementJeu.addEventListener('click', function () {
+                    joueur1.recuperationDuSurnom()
+                    partie1.afficherIndice()
+                    partie1.afficherTabPremiereFois()
+                })
 
-        bouttonValideChoix.addEventListener('click', function () {
-            partie1.algorithmeDunTour()
-        })
+                bouttonValideChoix.addEventListener('click', function () {
+                    partie1.algorithmeDunTour()
+                })
 
-        bouttonRejouer.addEventListener('click', function () {
-            partie1.rejouer()
-        })
-        
-        bouttonQuitter.addEventListener('click', function () {
-            partie1.rejouer()
-        })
-}
+                bouttonRejouer.addEventListener('click', function () {
+                    partie1.rejouer()
+                })
+                
+                bouttonQuitter.addEventListener('click', function () {
+                    partie1.rejouer()
+                })
+})
